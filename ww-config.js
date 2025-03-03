d3ce3ad0baef3659ebf5f3da5c9964ccea8c0719
neha-infi <neha@infiwebsolutions.com>
2025-03-03 16:04:41 +0530

    checking test

--- ww-config.js
+++ ww-config.js
@@ -1,17 +1,19 @@
 export default {
   editor: {
     label: {
-      en: "pdf viewer",
+      en: "PDF viewer",
     },
+    icon: 'document-report',
   },
   properties: {
-    textColor: {
-      label: {
-        en: "URL",
+    pdf: {
+      label: { en: "PDF Url", fr: "PDF Url" },
+      type: "Text",
+      options: {
+        placeholder: "PDF url",
       },
-      defaultValue: "#F23636",
-      type: "String",
-      defaultValue: "",
+      defaultValue:
+        "https://weweb-staging.s3.amazonaws.com/designs/1fb3c298-9f4f-4b49-9661-35a8eee7941b/files/Focussurcatnat.pdf",
       bindable: true,
     },
   },

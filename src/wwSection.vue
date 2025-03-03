4805e257d4c770660527ae3192b635e682f76c6e
neha-infi <neha@infiwebsolutions.com>
2025-03-03 15:50:41 +0530

    Initial commit

---
+++ src/wwSection.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="my-section" >
+    <h1 :style="textStyle">My Title</h1>
+    <p>
+      test
+    </p>
+    <div id="app">
+      <WebViewer :path="`${publicPath}`" url="https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import WebViewer from './components/WebViewer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    WebViewer
+  },
+  data () {
+    return {
+      publicPath: "/dist/public/pdfjsexpress/"
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.my-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 200px;
+  h1 {
+    font-size: 32px;
+  }
+  p {
+    margin-top: 12px;
+  }
+}
+</style>
---
+++ ww-config.js
@@ -0,0 +1,16 @@
+export default {
+  editor: {
+    label: {
+      en: "pdf viewer",
+    },
+  },
+  properties: {
+    textColor: {
+      label: {
+        en: "Text color",
+      },
+      type: "Color",
+      defaultValue: "#F23636",
+    },
+  },
+};
